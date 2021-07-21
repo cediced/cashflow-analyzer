69528b625dc9977b46bbeeb6333488159299a176
cedric <acquaviva@hotmail.fr>
2021-07-21 20:42:15 +0200

    refactor with classes Revenus and Expenses finished

--- cashflow_analyzer/calculate_cashflow.py
+++ cashflow_analyzer/calculate_cashflow.py
@@ -37,59 +37,41 @@ def get_grouped_expenses_every_year(data):
 
 
 def get_expenses_each_year(data):
-    return get_transactions_by_year("expenses", data)
-
-
-def get_grouped_transactions_by_year(type_of_transaction, data):
-    day = SCHEMA["day"]
-    amount = SCHEMA["amount"]
-    payer = "Beguenstigter/Zahlungspflichtiger"
-    data = data[[day, amount, payer]]
-    data.index = pd.to_datetime(data[SCHEMA["day"]], format='%d.%m.%y')
-    data[amount] = data[amount].str.replace(",", ".").astype(float)
-
-    if type_of_transaction == "revenus":
-        data = data[data[amount] > 0]
-    else:
-        data = data[data[amount] < 0]
-
-    return data.groupby(by=[data.index.year, payer]).sum().reset_index()
-
-
-def get_transactions_by_year(type_of_transaction, data):
-    day = SCHEMA["day"]
-    amount = SCHEMA["amount"]
-    data = data[[day, amount]]
-    data.index = pd.to_datetime(data[SCHEMA["day"]], format='%d.%m.%y')
-    data[amount] = data[amount].str.replace(",", ".").astype(float)
-
-    data = filter_transaction(type_of_transaction, amount, data)
-
-    return data.groupby(by=[data.index.year]).sum().reset_index()
-
-
-def filter_transaction(type_of_transaction, amount_col_name, data):
-    if type_of_transaction == "revenus":
-        data = data[data[amount_col_name] > 0]
-    else:
-        data = data[data[amount_col_name] < 0]
-    return data
+    return Expenses(data).get_transactions_by_year()
 
 
 class Revenus:
     def __init__(self, data):
         self.data = data
+        self.day = SCHEMA["day"]
+        self.amount = SCHEMA["amount"]
+        self.payer = "Beguenstigter/Zahlungspflichtiger"
+
+    def filter_transaction(self, data):
+        return data[data[SCHEMA["amount"]] > 0]
 
     def get_grouped_by_year(self):
-        return get_grouped_transactions_by_year("revenus", self.data)
+        data = self.data[[self.day, self.amount, self.payer]]
+        data.index = pd.to_datetime(data[self.day], format='%d.%m.%y')
+        data[self.amount] = data[self.amount].str.replace(",", ".").astype(float)
+
+        data = self.filter_transaction(data)
+
+        return data.groupby(by=[data.index.year, self.payer]).sum().reset_index()
 
     def get_transactions_by_year(self):
-        return get_transactions_by_year("revenus", self.data)
+        data = self.data[[self.day, self.amount]]
+        data.index = pd.to_datetime(data[SCHEMA["day"]], format='%d.%m.%y')
+        data[self.amount] = data[self.amount].str.replace(",", ".").astype(float)
+
+        data = self.filter_transaction(data)
+
+        return data.groupby(by=[data.index.year]).sum().reset_index()
 
 
-class Expenses:
+class Expenses(Revenus):
     def __init__(self, data):
-        self.data = data
+        super().__init__(data)
 
-    def get_grouped_by_year(self):
-        return get_grouped_transactions_by_year("expenses", self.data)
+    def filter_transaction(self, data):
+        return data[data[SCHEMA["amount"]] < 0]
