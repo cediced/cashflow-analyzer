05f71cfd0892e9158bbccaf5e5e181b006e5e2dd
cedric <acquaviva@hotmail.fr>
2021-07-30 15:47:10 +0200

    refactor tests for loaders

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -5,35 +5,37 @@ import pytest
 from cashflow_analyzer import loader
 from cashflow_analyzer.transactions import SCHEMA, TransactionsAnalyser
 
+
 DEUTSCHE_BANK_DF = pd.DataFrame(
-    {
-        "Booking date": ["01/27/2021", "06/30/2021"],
-        "Beneficiary / Originator": ["EDEKA", None],
-        "Payment Details": [None, "multiplus"],
-        "Debit": [None, "-5.0"],
-        "Credit": ["2,160.5", None]
-    }
-)
+        {
+            "Booking date": ["01/27/2021", "06/30/2021"],
+            "Beneficiary / Originator": ["EDEKA", None],
+            "Payment Details": [None, "multiplus"],
+            "Debit": [None, "-5.0"],
+            "Credit": ["2,160.5", None]
+        }
+    )
+
 
 SPARKASSE_DF = pd.DataFrame(
-    {
-        "Booking date": ["01/27/2021", "30/06/2021"],
-        "Beneficiary / Originator": ["EDEKA", None],
-        "Payment Details": [None, "multiplus"],
-        "Debit": [None, "-5.0"],
-        "Credit": ["2,160.5", None]
-    }
-)
+            {
+                "Buchungstag": ["27.01.21", "30.06.21"],
+                "Beguenstigter/Zahlungspflichtiger": ["EDEKA", "multiplus"],
+                "Betrag": ["2160,5", "-5,0"],
+            }
+        )
 
 
-@pytest.mark.parametrize("loader_type, input_df",
+@pytest.mark.parametrize("convertor, input_df",
                          [
                              (loader.db_convertor, DEUTSCHE_BANK_DF),
                              (loader.sparkasse_convertor, SPARKASSE_DF)
                          ])
-def test_change_deutsche_bank_columns(loader_type, input_df):
-    df = loader.db_convertor(input_df)
+def test_change_deutsche_bank_columns(convertor, input_df):
+
+    df = convertor(input_df)
     expected = pd.DataFrame({
+
         SCHEMA["day"]: ["27.01.2021", "30.06.2021"],
         SCHEMA["amount"]: [2160.5, -5.0],
         SCHEMA["payer"]: ["EDEKA", "multiplus"]
