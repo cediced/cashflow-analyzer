f6d82928bc8bbe121b73da5a04d162648432faa2
cedric <acquaviva@hotmail.fr>
2021-08-04 21:37:14 +0200

    refactor graph shows

--- cashflow_analyzer/__main__.py
+++ cashflow_analyzer/__main__.py
@@ -7,61 +7,58 @@ import argparse
 
 import matplotlib.pyplot as plt
 
+from collections import namedtuple
 
-def plot_report_over_the_years(request):
-    fig, ax = plt.subplots(figsize=(10, 5), tight_layout=True)
-    ax.plot(result["years"], result["Betrag"], '-X')
-    ax.set_xlabel("years")
-    ax.set_ylabel(f"{request.transaction_type} in euro")
-    ax.set_title(f"{request.transaction_type} over the years")
+Model = namedtuple("Model", "fig_size, amount, x_axis, x_label, y_label, title")
+
+
+def graph_interface(data, transaction_type, step):
+    step_title = "years" if step == "yearly" else "months"
+    type = "cashflow" if transaction_type == "all" else transaction_type
+    x_axis = data[step_title] if step == "yearly" else data["years"].astype(str) + "-" + data["months"].astype(str)
+
+    return Model(fig_size=(10, 5),
+                 amount=data[SCHEMA["amount"]],
+                 x_axis=x_axis,
+                 x_label=step_title,
+                 y_label=f"{type} in euro",
+                 title=f"{type} over the {step_title}")
+
+
+def plot_report(model: Model):
+    fig, ax = plt.subplots(figsize=model.fig_size, tight_layout=True)
+    ax.plot(model.x_axis, model.amount, '-X')
+    ax.set_xlabel(model.x_label)
+    ax.set_ylabel(model.y_label)
+    ax.set_title(model.title)
     plt.grid()
-    plt.xticks(result["years"])
-    plt.show()
-
-
-def plot_report_over_the_month(request):
-    result["date"] = result["years"].astype(str) + "-" + result["months"].astype(str)
-    fig, ax = plt.subplots(figsize=(10, 5), tight_layout=True)
-
-    ax.plot(result["date"], result["Betrag"], '-X')
-    ax.set_xlabel("months")
-    ax.set_ylabel(f"{request.transaction_type} in euro")
-    ax.set_title(f"{request.transaction_type} over the months")
-    plt.grid()
-    plt.xticks(result["date"], rotation='vertical')
-    plt.show()
+    plt.xticks(model.x_axis, rotation='vertical')
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='analyse account.')
-    parser.add_argument('-t', '--transaction-type', type=str,
-                        help=f'type of transaction from {list(TRANSACTIONS_TYPES.keys())}')
-    parser.add_argument('-s', '--step', default="yearly", type=str,
-                        help=f'monthly or yearly')
-
-    parser.add_argument('-g', '--grouped', default=False, type=bool,
-                        help=f'per group of payers/receiver')
-
-    parser.add_argument('-p', '--payers', default="", type=str, nargs='+',
-                        help=f'receiver/payer')
-
-    args = parser.parse_args()
-    request = AnalyseRequest(**vars(args))
-    v = AnalyseRequestValidator()
-    v.validate(request)
 
     PATH_DATA = r"../data/20210613-101728580-umsatz (1).CSV"
     # PATH_DATA = r"../data/Transactions_380_633854500_20210725_163342.csv"
 
     data = sparkasse_convertor(get_data(PATH_DATA))
 
-    t = TransactionsAnalyser(transaction_type=request.transaction_type,
-                             step=request.step,
-                             is_grouped=request.grouped,
-                             selections=request.payers)
-    result = t.sum(data)
-    print(result)
-    if len(t.errors) != 0:
-        print(t.errors)
+    requests = [AnalyseRequest(transaction_type="all"),
+                AnalyseRequest(transaction_type="expenses"),
+                AnalyseRequest(transaction_type="revenues"),
+                AnalyseRequest(transaction_type="all", step="monthly"),
+                AnalyseRequest(transaction_type="expenses", step="monthly"),
+                AnalyseRequest(transaction_type="revenues", step="monthly")
+                ]
 
-    plot_report_over_the_month(request)
+    for request in requests:
+        v = AnalyseRequestValidator()
+        v.validate(request)
+
+        t = TransactionsAnalyser(transaction_type=request.transaction_type,
+                                 step=request.step,
+                                 is_grouped=request.grouped,
+                                 selections=request.payers)
+        result = t.sum(data)
+
+        plot_report(graph_interface(result, request.transaction_type, request.step))
+        plt.show()
